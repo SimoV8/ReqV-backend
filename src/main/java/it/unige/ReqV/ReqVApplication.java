270c57770a93c3e4931d07ddf73d07e9c28b6607
Simone Vuotto <simov8@gmail.com>
2017-12-21 11:34:49 +0100

    Init

---
+++ src/main/java/it/unige/ReqV/ReqVApplication.java
@@ -0,0 +1,12 @@
+package it.unige.ReqV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReqVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReqVApplication.class, args);
+	}
+}
