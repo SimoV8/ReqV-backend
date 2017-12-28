5df6a2d2459238ecdeb47562fbbb4adc644b2f2a
Simone Vuotto <simov8@gmail.com>
2017-12-28 20:43:59 +0100

    Add unit tests

---
+++ src/test/java/it/unige/ReqV/user/AuthorizationTests.java
@@ -0,0 +1,169 @@
+package it.unige.ReqV.user;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import it.unige.ReqV.ReqVApplication;
+import net.minidev.json.JSONObject;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static it.unige.ReqV.security.SecurityConstants.TOKEN_PREFIX;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ReqVApplication.class)
+@ActiveProfiles("test")
+public class AuthorizationTests {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Autowired
+    public UserRepository repository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Before
+    public void setUp() {
+        User admin = new User("admin", bCryptPasswordEncoder.encode("1234"), "admin@test.it");
+
+        repository.deleteAll();
+        repository.save(admin);
+
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        restTemplate.getRestTemplate().setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
+    }
+
+    @Test
+    public void testLoginSuccess() {
+
+        ResponseEntity<String> response = login(restTemplate,"admin", "1234");
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertNotNull(response.getHeaders().get("Authorization"));
+        assertThat(response.getHeaders().get("Authorization").size(), is(1));
+
+        // Check token is correct
+        String token = response.getHeaders().getFirst("Authorization");
+        assertThat(token, containsString(TOKEN_PREFIX));
+        token = token.replace(TOKEN_PREFIX, "");
+        int i = token.lastIndexOf('.');
+        String untrustedJwtString = token.substring(0, i+1);
+        Jwt<Header,Claims> untrusted = Jwts.parser().parseClaimsJwt(untrustedJwtString);
+        assertThat(untrusted.getBody().getSubject(), is("admin"));
+
+    }
+
+    @Test
+    public void testLoginFailWithWrongPassword() {
+
+        ResponseEntity<String> response = login(restTemplate,"admin", "abc");
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.UNAUTHORIZED));
+        assertThat(response.getHeaders().get("Authorization"), nullValue());
+    }
+
+    @Test
+    public void testLoginFailWithWrongUsername() {
+
+        ResponseEntity<String> response = login(restTemplate,"wrong", "1234");
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.UNAUTHORIZED));
+        assertThat(response.getHeaders().get("Authorization"), nullValue());
+    }
+
+    @Test
+    public void testLoginFailWithoutPassword() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        JSONObject body = new JSONObject();
+        body.put("username", "admin");
+
+        HttpEntity<String> request = new HttpEntity<>(body.toString(), headers);
+
+        ResponseEntity<String> response = restTemplate.exchange("/login", HttpMethod.POST, request, String.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.UNAUTHORIZED));
+        assertThat(response.getHeaders().get("Authorization"), nullValue());
+    }
+
+    @Test
+    public void testLoginFailWithoutBody() {
+
+        ResponseEntity<String> response = restTemplate.postForEntity("/login", "", String.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.UNAUTHORIZED));
+        assertThat(response.getHeaders().get("Authorization"), nullValue());
+    }
+
+    @Test
+    public void testLoginFailWrongMethod() {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        JSONObject body = new JSONObject();
+        body.put("username", "admin");
+        body.put("password", "1234");
+
+        HttpEntity<String> request = new HttpEntity<>(body.toString(), headers);
+
+        ResponseEntity<String> response = restTemplate.exchange("/login", HttpMethod.GET, request, String.class);
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.FORBIDDEN));
+        assertThat(response.getHeaders().get("Authorization"), nullValue());
+
+        response = restTemplate.exchange("/login", HttpMethod.PUT, request, String.class);
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.FORBIDDEN));
+        assertThat(response.getHeaders().get("Authorization"), nullValue());
+
+        response = restTemplate.exchange("/login", HttpMethod.DELETE, request, String.class);
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.FORBIDDEN));
+        assertThat(response.getHeaders().get("Authorization"), nullValue());
+    }
+
+    public static ResponseEntity<String> login(TestRestTemplate restTemplate, String username, String password) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        JSONObject body = new JSONObject();
+        body.put("username", username);
+        body.put("password", password);
+
+        HttpEntity<String> request = new HttpEntity<>(body.toString(), headers);
+
+        return restTemplate.exchange("/login", HttpMethod.POST, request, String.class);
+    }
+
+    public static HttpHeaders headerWithAuthorizationToken(TestRestTemplate restTemplate, String username, String password) {
+
+        ResponseEntity<String> response = login(restTemplate, username, password);
+        if(response.getStatusCode() == HttpStatus.OK && response.getHeaders().containsKey("Authorization")) {
+
+            HttpHeaders header = new HttpHeaders();
+            header.setContentType(MediaType.APPLICATION_JSON);
+            header.set("Authorization", response.getHeaders().getFirst("Authorization"));
+            return header;
+        }
+
+        return null;
+    }
+
+}
---
+++ src/test/java/it/unige/ReqV/user/UserControllerTests.java
@@ -0,0 +1,271 @@
+package it.unige.ReqV.user;
+
+import it.unige.ReqV.ReqVApplication;
+import net.minidev.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static it.unige.ReqV.user.AuthorizationTests.headerWithAuthorizationToken;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ReqVApplication.class)
+@ActiveProfiles("test")
+public class UserControllerTests {
+
+    @Value("${local.server.port}")
+    int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    public UserRepository repository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    User user1, user2;
+
+    @Before
+    public void setUp() {
+
+        user1 = new User("admin", bCryptPasswordEncoder.encode("1234"), "admin@test.it");
+        user2 = new User("test", bCryptPasswordEncoder.encode("abcde"), "test@test.it");
+
+
+        repository.deleteAll();
+        user1 = repository.save(user1);
+        user2 = repository.save(user2);
+
+    }
+
+    @Test
+    public void testGetUserDetails() {
+        HttpHeaders header = headerWithAuthorizationToken(restTemplate, user1.getUsername(), "1234");
+        HttpEntity<String> request = new HttpEntity<>("", header);
+        ResponseEntity<User> response = restTemplate.exchange("/user", HttpMethod.GET, request, User.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertEquals(response.getBody().getId(), user1.getId());
+        assertEquals(response.getBody().getUsername(), user1.getUsername());
+        assertEquals(response.getBody().getEmail(), user1.getEmail());
+        assertThat(response.getBody().getPassword(), isEmptyOrNullString());
+    }
+
+    @Test
+    public void testGetUserDetailsNotAuthorized() {
+        ResponseEntity<User> response = restTemplate.getForEntity("/user", User.class);
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.FORBIDDEN));
+        assertThat(response.getBody().getId(), nullValue());
+        assertThat(response.getBody().getUsername(), nullValue());
+        assertThat(response.getBody().getPassword(), nullValue());
+        assertThat(response.getBody().getEmail(), nullValue());
+    }
+
+    @Test
+    public void testGetUserDetailsById() {
+        HttpHeaders header = headerWithAuthorizationToken(restTemplate, user1.getUsername(), "1234");
+        HttpEntity<String> request = new HttpEntity<>("", header);
+        String url = "/user/" + user2.getId();
+        ResponseEntity<User> response = restTemplate.exchange(url, HttpMethod.GET, request, User.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertEquals(response.getBody().getId(), user2.getId());
+        assertEquals(response.getBody().getUsername(), user2.getUsername());
+        assertEquals(response.getBody().getEmail(), user2.getEmail());
+        assertThat(response.getBody().getPassword(), isEmptyOrNullString());
+    }
+
+    @Test
+    public void testGetUserDetailsByIdNotAuthorized() {
+        ResponseEntity<User> response = restTemplate.getForEntity("/user/" + user1.getId(), User.class);
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.FORBIDDEN));
+        assertThat(response.getBody().getId(), nullValue());
+        assertThat(response.getBody().getUsername(), nullValue());
+        assertThat(response.getBody().getPassword(), nullValue());
+        assertThat(response.getBody().getEmail(), nullValue());
+
+    }
+
+    @Test
+    public void testEditUserEmail() {
+        HttpHeaders header = headerWithAuthorizationToken(restTemplate, user1.getUsername(), "1234");
+
+        JSONObject body = new JSONObject();
+        body.put("id", user1.getId());
+        body.put("username", user1.getUsername());
+        body.put("email", "new_email@test.com");
+        HttpEntity<String> request = new HttpEntity<>(body.toString(), header);
+        ResponseEntity<User> response = restTemplate.exchange("/user", HttpMethod.POST, request, User.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(response.getBody().getId(), is(user1.getId()));
+        assertThat(response.getBody().getUsername(), is("admin"));
+        assertThat(response.getBody().getEmail(), is("new_email@test.com"));
+        assertThat(response.getBody().getPassword(), nullValue());
+
+        User updatedUser = repository.findById(user1.getId());
+        assertThat(response.getBody().getUsername(), equalTo(updatedUser.getUsername()));
+        assertThat(response.getBody().getEmail(), equalTo(updatedUser.getEmail()));
+        assertTrue(bCryptPasswordEncoder.matches("1234", updatedUser.getPassword()));
+    }
+
+    @Test
+    public void testEditUserEmailAndPassword() {
+        HttpHeaders header = headerWithAuthorizationToken(restTemplate, user1.getUsername(), "1234");
+
+        JSONObject body = new JSONObject();
+        body.put("id", user1.getId());
+        body.put("username", user1.getUsername());
+        body.put("email", "new_email@test.com");
+        body.put("password", "new_password");
+        HttpEntity<String> request = new HttpEntity<>(body.toString(), header);
+        ResponseEntity<User> response = restTemplate.exchange("/user", HttpMethod.POST, request, User.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(response.getBody().getId(), is(user1.getId()));
+        assertThat(response.getBody().getUsername(), is("admin"));
+        assertThat(response.getBody().getEmail(), is("new_email@test.com"));
+        assertThat(response.getBody().getPassword(), nullValue());
+
+
+        User updatedUser = repository.findById(user1.getId());
+        assertThat(updatedUser.getUsername(), equalTo(response.getBody().getUsername()));
+        assertThat(updatedUser.getEmail(), equalTo(response.getBody().getEmail()));
+        assertTrue(bCryptPasswordEncoder.matches("new_password", updatedUser.getPassword()));
+    }
+
+    @Test
+    public void testEditUserDetailsNotAuthorized() {
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(MediaType.APPLICATION_JSON);
+
+        JSONObject body = new JSONObject();
+        body.put("username", "admin");
+        body.put("email", "new_email@test.com");
+        HttpEntity<String> request = new HttpEntity<>(body.toString(), header);
+
+        ResponseEntity<String> response = restTemplate.exchange("/user", HttpMethod.POST, request, String.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.FORBIDDEN));
+    }
+
+    @Test
+    public void testSignUp() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        JSONObject body = new JSONObject();
+        body.put("username", "simone");
+        body.put("password", "1234");
+        body.put("email", "simone@test.com");
+
+        HttpEntity<String> request = new HttpEntity<>(body.toString(), headers);
+
+        ResponseEntity<User> response = restTemplate.postForEntity("/user/sign-up", request, User.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+    }
+
+    @Test
+    public void testSignUpWithoutUsername() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        JSONObject body = new JSONObject();
+        body.put("password", "1234");
+        body.put("email", "simone@test.com");
+
+        HttpEntity<String> request = new HttpEntity<>(body.toString(), headers);
+
+        ResponseEntity<User> response = restTemplate.postForEntity("/user/sign-up", request, User.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.BAD_REQUEST));
+    }
+
+    @Test
+    public void testSignUpWithoutPassword() {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        JSONObject body = new JSONObject();
+        body.put("username", "simone");
+        body.put("email", "simone@test.com");
+
+        HttpEntity<String> request = new HttpEntity<>(body.toString(), headers);
+
+        ResponseEntity<User> response = restTemplate.postForEntity("/user/sign-up", request, User.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.BAD_REQUEST));
+    }
+
+    @Test
+    public void testSignUpWithoutEmail() {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        JSONObject body = new JSONObject();
+        body.put("username", "simone");
+        body.put("password", "1234");
+
+        HttpEntity<String> request = new HttpEntity<>(body.toString(), headers);
+
+        ResponseEntity<User> response = restTemplate.postForEntity("/user/sign-up", request, User.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.BAD_REQUEST));
+    }
+
+    @Test
+    public void testSignUpWithoutBody() {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+
+        ResponseEntity<User> response = restTemplate.postForEntity("/user/sign-up", request, User.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.BAD_REQUEST));
+    }
+
+    /**
+     * Check that it's not possible to create a new user with an existing username
+     */
+    @Test
+    public void testSignUpWithExistingUsername() {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        JSONObject body = new JSONObject();
+        body.put("username", user1.getUsername());
+        body.put("password", "1234");
+        body.put("email", "simone@test.com");
+
+        HttpEntity<String> request = new HttpEntity<>(body.toString(), headers);
+
+        ResponseEntity<User> response = restTemplate.postForEntity("/user/sign-up", request, User.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.BAD_REQUEST));
+    }
+
+
+
+
+
+
+}
